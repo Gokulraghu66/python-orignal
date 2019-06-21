50ef8d3f81a08b0b747fa5729ee2f9122d581571
gokulgene <51950952+gokulgene@users.noreply.github.com>
2019-06-21 15:09:05 +0530

    Update 3.py

--- 3.py
+++ 3.py
@@ -1,7 +1,6 @@
 ch = input()
 
-if(ch=='A' or ch=='a' or ch=='E' or ch =='e' or ch=='I'
- or ch=='i' or ch=='O' or ch=='o' or ch=='U' or ch=='u'):
+if(ch=='E' or ch =='e' or ch=='I' or ch=='i' or ch=='O' or ch=='o' or ch=='U' or ch=='u' or ch=='A' or ch=='a':
     print("Vowel")
 else:
     print("Consonant")
