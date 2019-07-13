df9e3171b7bc71e99847755efeed651fe357d2c8
gokulgene <51950952+gokulgene@users.noreply.github.com>
2019-07-13 11:29:36 +0530

    Create proset23rd.py

---
+++ proset23rd.py
@@ -0,0 +1,13 @@
+asd,ssd=map(int,input().split())
+kaat=[]
+shut=[]
+shat=[int(p) for p in input().split() ]
+for g in range(0,ssd):
+    usat,unt=map(int,input().split())
+    for h in range(usat-1 ,unt):
+        shut.append(shat[h])
+    xaat=sorted(shut)
+    kaat.append(min(shut))
+    del shut[:]
+for o in range(0,len(kaat)):
+    print(kaat[o])
